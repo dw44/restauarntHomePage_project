fa77bdb549fb46c3698de190ee1c929c96ae3202
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-05-20 22:08:13 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # restauarntHomePage_project
+
+Part of The Odin Project curriculum. 
+
+Project details: https://www.theodinproject.com/courses/javascript/lessons/restaurant-page
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+};
+
+module.exports = config;
